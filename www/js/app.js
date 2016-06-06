5b7fec0b555beb58d1186bd55596b5976e6af681
Nicolas Rostov <NicolasRostov@gmail.com>
2016-06-06 04:21:55 +0300

    UI update, files list, download logic improved

--- www/js/app.js
+++ www/js/app.js
@@ -24,18 +24,107 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 })
 
-.controller('DownloadCtrl', function($scope) {
+.factory("$fileFactory", function($q) {
 
-  var __i=0, __a = 0;
+    var File = function() { };
 
-  onTrigger = function(notification) {
-    console.dir(notification);
+    File.prototype = {
+
+        getEntry: function(path) {
+            var deferred = $q.defer();
+            window.resolveLocalFileSystemURL(path, function(fileSystem) {
+                deferred.resolve(fileSystem);
+            }, function(error) {
+              console.log(error);
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntriesAtRoot: function() {
+            var deferred = $q.defer();
+            window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+                var directoryReader = fileSystem.root.createReader();
+                directoryReader.readEntries(function(entries) {
+                    deferred.resolve(entries);
+                }, function(error) {
+                    deferred.reject(error);
+                });
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        },
+
+        getEntries: function(path) {
+            var deferred = $q.defer();
+            window.resolveLocalFileSystemURL(path, function(fileSystem) {
+                  if(fileSystem.isFile) {
+                      deferred.resolve(null);
+                  }
+                  else {
+                    var directoryReader = fileSystem.createReader();
+                    directoryReader.readEntries(function(entries) {
+                        deferred.resolve(entries);
+                    });
+                  }
+            }, function(error) {
+                deferred.reject(error);
+            });
+            return deferred.promise;
+        }
+
+    };
+
+    return File;
+
+})
+
+.controller('DownloadCtrl', function($scope, $ionicPlatform, $fileFactory) {
+  var fs = new $fileFactory();
+
+      $ionicPlatform.ready(function() {
+          updateFiles();
+      });
+
+  updateFiles = function() {
+        fs.getEntriesAtRoot().then(function(result) {
+            $scope.files = result;
+        }, function(error) {
+            console.error(error);
+        });
   };
+
+  $scope.getContents = function(path) {
+    // user did tap on some entry. If it's a directory,
+    // getEntries will return list of files and we show them.
+    // If it's a file, getEntries will return null and we'll remove it.
+      fs.getEntries(path).then(function(result) {
+        if(result) {
+              $scope.files = result;
+              $scope.files.unshift({name: "[parent]"});
+              var parentDir = path.substring(0, path.lastIndexOf('/'));
+              parentDir = parentDir.substring(0, parentDir.lastIndexOf('/')+1);
+              fs.getEntry(parentDir).then(function(result) {
+                  result.name = "[parent]";
+                  $scope.files[0] = result;
+              });
+        }
+        else {
+              fs.getEntry(path).then(function(result) {
+                  result.remove();
+                  updateFiles();
+              });
+        }
+      });
+  };
+
 
   download = function(url, success, failure, progress) {
     var fileName = url.substring(url.lastIndexOf('/') + 1);
 
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+        updateFiles();
         fileSystem.root.getFile(fileName, { create: true }, function (targetFile) {
             var downloader = new BackgroundTransfer.BackgroundDownloader();
             // Create a new download operation.
@@ -53,12 +142,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
               });
         });
     });
-  }
+  };
 
   $scope.downloadAction = function() {
     window.progressBar.value = 0;
     download("http://cdn.wall-pix.net/albums/art-space/00030109.jpg",
               function(targetFile) {
+                updateFiles();
                 console.log("download complete: " + targetFile.toURL());
               },
               function(error) {
@@ -74,14 +164,17 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   $scope.pauseAction = function() {
         // pause downloading
         $scope.download.pause();
+        updateFiles();
   };
 
   $scope.resumeAction = function() {
         // resume downloading
         $scope.download.resume();
+        updateFiles();
   };
 
   $scope.abortAction = function() {
+    if($scope.download) {
         // stop downloading
         $scope.download.stop();
         // clear progress bar
@@ -89,24 +182,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         // remove local file
         $scope.download.resultFile.remove(function(){
           // The file has been removed succesfully
+          updateFiles();
         },function(error){
             // Error deleting the file
         },function(){
            // The file doesn't exist
         });
+    }
   };
 
   $scope.ntfyAction = function() {
+        cordova.plugins.notification.local.registerPermission(function (granted) {
+                        console.log(granted);
+                    });
         var fetcher = window.BackgroundFetch;
 
-        cordova.plugins.notification.local.on("trigger", onTrigger);
         // Your background-fetch handler.
         var fetchCallback = function() {
             console.log('BackgroundFetch initiated');
             cordova.plugins.notification.local.schedule({
                 id: 1,
-                title: "BackgroundFetch initiated",
-                text: "File: "+url
+                title: "Background download started",
+                text: ""
             });
 
           	var url = "http://www.keithandthegirl.com/vip/download/shows/mnik/MNIK-2016-05-27.mp3?a=14-bfc1217d9d82361a802b";
@@ -132,7 +229,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
                         fetcher.finish();
                       },
                       function(progress) {
-
+                          console.log("Background progress: " + progress*100);
+                          window.progressBar.value = progress *100;
                       });
         }
         var failureCallback = function() {
